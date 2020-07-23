24e577bf6d3bcffd6e31293e6741c7e1cf44ac10
yanzhen <you@example.com>
2020-07-23 09:42:41 +0800

    develop B3

--- test1/test1/AppDelegate.h
+++ test1/test1/AppDelegate.h
@@ -18,6 +18,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (nonatomic, strong) NSString *B1;
 @property (nonatomic, strong) NSString *B2;
+@property (nonatomic, strong) NSString *B3;
 
 @end
 
