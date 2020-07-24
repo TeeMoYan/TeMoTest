3639ad470f4e2011951ef62674809f6eb84ec357
yanzhen <you@example.com>
2020-07-24 10:12:37 +0800

    添加a

--- test1/test1/main.m
+++ test1/test1/main.m
@@ -5,7 +5,7 @@
 //  Created by 闫振 on 2020/6/17.
 //
 //  Copyright (C) 2011-2020 ShenZhen iBOXCHAIN Information Technology Co.,Ltd. 
-//                             All rights reserved.
+//                             All rights reserved.fd
 //
 //      This  software  is  the  confidential  and  proprietary  information  of
 //  iBOXCHAIN  Company  of  China. ("Confidential Information"). You  shall  not
